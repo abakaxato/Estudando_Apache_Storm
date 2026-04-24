34a038e9c01c78ad8f86f57581f91efa32026986
Abakaxato <cauakika@gmail.com>
2026-04-24 22:17:31 +0000

    Subindo atualizacao da classe YahooBolt

--- src/main/java/YahooBolt.java
+++ src/main/java/YahooBolt.java
@@ -3,6 +3,7 @@ import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
 import java.util.Map;
 
@@ -11,18 +12,25 @@ public class YahooBolt extends BaseBasicBolt {
     public final void prepare(Map stormConf, TopologyContext context){}
 
     public void execute(Tuple input, BasicOutputCollector collector) {
-    String empresa = input.getValue(0).toString();
-    String timestamp = input.getString(1);
-    Double preco = (Double) input.getValueByField("preco");
-    Double fechamentoAnterior = input.getDoubleByField("fechamentoPrevio");
-    boolean ganho = true;
+        String empresa = input.getValue(0).toString();
+        String timestamp = input.getString(1);
+        Double preco = (Double) input.getValueByField("preco");
+        Double fechamentoAnterior = input.getDoubleByField("fechamentoPrevio");
+        boolean ganho = true;
 
-    if (preco <= fechamentoAnterior){
-        ganho = false;
-        System.out.println("Ocorreu perda ou manteve o valor da ação");
-    }
-
+        if (preco <= fechamentoAnterior){
+            ganho = false;
+            System.out.println("vish, ganhasse nada de diferença");
+        }else{
+            System.out.println("Ae sim, teve um ganho !");
+            System.out.println(empresa);
+            System.out.println(timestamp);
+            System.out.println(preco);
+            System.out.println(ganho);
+            collector.emit(new Values(empresa,timestamp,preco,ganho));
+        }
     }
 
     public void declareOutputFields(OutputFieldsDeclarer declarer) {}
+    public  void cleanup(){}
 }
